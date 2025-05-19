88371fb20d4d76ff342431cd00d33dccbc279651
Chanda Mounica <pvryar.4@gmail.com>
2025-05-20 02:16:05 +0530

    day 4 update

---
+++ Functions/4functionscript.js
@@ -0,0 +1,29 @@
+
+
+
+let company = function(){
+    console.log(manager);
+}
+
+console.log(company);
+console.dir(company);
+
+
+// let sum = function sum(x, y){
+//     return(x+y);
+// }
+// console.log(sum(3, 6));
+
+
+//Function declaration
+function personDetails(){
+let customerAge = 10;
+console.log(customerAge);  //statement 1
+
+
+let car = "skoda";
+console.log(car);   //statement 2
+}
+
+personDetails(); //function calling
+
---
+++ Functions/styles.js
@@ -0,0 +1,47 @@
+console.log("Mounica");
+
+console.log("%c Mounica", "color:black");
+
+
+console.log("%c Mounica", "background-color:yellowgreen");
+
+console.log("%c      ", "background-color:red");
+
+
+//function calling
+
+function redcolor(){
+    console.log("%c      ", "background-color:red");
+}
+
+redcolor();
+
+//if red color is common but i want the red color in three sections without adding everytime
+
+function redcolor(){
+    console.log("%c     ","background-color:red");
+
+}
+
+function firstSection(){
+    console.log("%c      ", "background-color:yellow");
+    console.log("%c      ", "background-color:blue");
+    redcolor();
+
+}
+firstSection();
+
+function secondSection(){
+    console.log("%c      ", "background-color:yellowgreen");
+    console.log("%c      ", "background-color:lightgray");
+    redcolor();
+
+}
+secondSection();
+function thirdSection(){
+    console.log("%c      ", "background-color:purple");
+    console.log("%c      ", "background-color:darkgray");
+    redcolor();
+
+}
+thirdSection();
