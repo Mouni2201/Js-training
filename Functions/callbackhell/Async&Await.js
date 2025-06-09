734b008082a5383bb15c14c25843003fc598ad09
Chanda Mounica <pvryar.4@gmail.com>
2025-06-09 07:27:59 +0530

    asunc&await

---
+++ Functions/callbackhell/Async&Await.js
@@ -0,0 +1,175 @@
+// function getData(){
+//     return "I hava a knowledge on Linux";
+// }
+// let bascic = getData();
+// console.log(bascic);      // I hava a knowledge on Linux
+
+
+//async keyword
+
+// async function getData(){
+//     return "I hava a knowledge on Linux";
+// }
+// let bascics = getData();
+// console.log(bascics);           // it will return promise
+//---------------------------------------------------------------
+
+// async function season(){
+//     return "Winter";
+// }
+// let seasonPromise = season();
+// seasonPromise.then((result)=>{
+//     console.log(result);                      //winter
+// })
+
+//----------------------------------------------------------------------
+
+// async function getauthorDetails(){
+//     return "Tagore";
+// }
+// getauthorDetails().then((data)=>{
+//     console.log("The Author of Geethanjali book is", data);
+// })
+
+// The Author of Geethanjali book is Tagore
+
+//---------------------------------------------------------------------
+
+//real promise
+
+// async function getauthorDetails(){
+//  return new Promise((resolve)=>{
+//     resolve("Tagore");
+//  })
+// }
+// getauthorDetails().then((data)=>{
+//     console.log("The Author of Geethanjali book is",data);
+// })
+// //-----------------------------------------------------------------------------
+
+// async function IPLSeasonDetails(){
+//    return new Promise((resolve, reject)=>{
+//     let Captain;
+//     if(Captain){
+//         resolve("Sachin Tendulkar");
+//     }
+
+//     else{
+//         reject("some error occurred");
+//     }
+//    })
+// }
+
+// IPLSeasonDetails().then((response)=>{
+//     console.log("The Captain of MumbaiIndains is", response);
+// }).catch((error)=>{
+//     console.log(error);
+//     //document.write(error);
+// })
+
+//-------------------------------------------------------------------
+
+// function IPLSeasonDetails(){
+//    return new Promise((resolve, reject)=>{
+//     console.log("Please wait 8 seconds");
+//     setTimeout(()=>{
+//         resolve("The captain of RCB is Kohli");
+//         //console.log("RCB");
+//     }, 8000);
+// })
+// }
+// async function IPLSeasonDetails2023(){
+//     console.log("Dhone is the captain of CSK");
+//     console.log("Gill is the captain of GT");
+//     // await IPLSeasonDetails().then((result)=>{
+//     //     console.log("Kohli is the caption of", result);
+//     //});
+
+//     let Details = await IPLSeasonDetails();
+//     console.log(Details);
+    
+//     console.log("Get all info");
+// }
+// IPLSeasonDetails2023();
+
+//==================================================================================
+
+// function movieRRRDetails(){
+//     return new Promise((resolve)=>{
+//         console.log("please wait 5 seconds");
+//         setTimeout(() => {
+//             resolve("The director of RRR Movie is SS.Rajamouli");
+//         }, 5000);
+//     })
+// }
+
+// function movieKalkiDetails(){
+//     return new Promise((resolve)=>{
+//         console.log("please wait 10 seconds");
+//         setTimeout(() => {
+//             resolve("The director of RRR Movie is NagAswin");
+//         }, 10000);
+//     })
+// }
+
+// async function movieDetails(){
+//     console.log("The RRR movie song get Oscar Award");
+//     console.log("The kalki movie has part2");
+
+//     let RRRDetails = await movieRRRDetails();
+//     console.log(RRRDetails);
+
+//     let KalkiDetails = await movieKalkiDetails();
+//     console.log(KalkiDetails);
+
+//     console.log("Received all the info for latest movies");
+// }
+
+// // movieDetails().then((result)=>{
+// //     console.log(result);  //it gives undefined
+// // })
+// movieDetails();
+
+//------------------------------------------------------------------------------
+
+function movieRRRDetails(){
+    return new Promise((resolve)=>{
+        console.log("please wait 5 seconds");
+        setTimeout(() => {
+            resolve("The director of RRR Movie is SS.Rajamouli");
+        }, 5000);
+    })
+}
+
+function movieKalkiDetails(){
+    return new Promise((resolve)=>{
+        console.log("please wait 10 seconds");
+        setTimeout(() => {
+            resolve("The director of RRR Movie is NagAswin");
+        }, 10000);
+    })
+}
+
+async function movieDetails(){
+    console.log("The RRR movie song get Oscar Award");
+    console.log("The kalki movie has part2");
+    console.time("Time start")
+    let RRRDetails = await movieRRRDetails();
+    console.log(RRRDetails);
+
+    let KalkiDetails = await movieKalkiDetails();
+    console.log(KalkiDetails);
+
+    console.log("Received the info for latest movies");
+    console.timeEnd("Time start")
+
+    return 'I got all the info from Sravani';
+}
+//here i gave at last return statement the movie details function goes to async function and the return comes the result.
+movieDetails().then((result)=>{
+    console.log(result);  
+})
+
+//------------------------------------------------------------------------------------------
+
+
---
+++ Functions/callbackhell/async&Await using API.js
@@ -0,0 +1,177 @@
+// let firstApiAddress = "https://jsonplaceholder.typicode.com/todos/1";  //fake api's
+
+// let secondApiAddress = "https://jsonplaceholder.typicode.com/todos/2";
+
+// let ALLApiAddress = "https://jsonplaceholder.typicode.com/todos/";
+
+
+// function FirstApi(){
+//     return fetch(firstApiAddress);      //promise
+
+// }
+// function SecondApi(){
+//     return fetch(secondApiAddress);     //promise
+    
+// }
+
+// function ThirdApi(){
+//     return fetch(ALLApiAddress);           //promise
+    
+// }
+
+// Promise.all([FirstApi(), SecondApi(), ThirdApi()]).then((AllResults)=>{
+//     console.log(AllResults);
+
+// })
+//--------------------------------------------------------------------------------------------
+
+// let firstApiAddress = "https://jsonplaceholder.typicode.com/todos/1";  //fake api's
+
+// let secondApiAddress = "https://jsonplaceholder.typicode.com/todos/2";
+
+// let ALLApiAddress = "https://jsonplaceholder.typicode.com/todos/";
+
+
+// function FirstApi(){
+//     return fetch(firstApiAddress);      //promise
+
+// }
+// function SecondApi(){
+//     return fetch(secondApiAddress);     //promise
+    
+// }
+
+// function ThirdApi(){
+//     return fetch(ALLApiAddress);           //promise
+    
+// }
+   
+// async function allApiResults(){
+//     let resultofFirstApi = await FirstApi();
+//     console.log(resultofFirstApi);
+    
+//     let resultofSecondApi = await SecondApi();
+//     console.log(resultofSecondApi);
+
+//     let resultofThirdApi = await ThirdApi();
+//     console.log(resultofThirdApi);
+
+//     console.log("Got info of all APIAddresses");
+//  }
+
+// allApiResults();
+//----------------------------------------------------------------------------------------------
+
+//for refernce
+// fetch(firstApiAddress).then((result)=>{
+//     //console.log(result); // it leads to promise
+//     return result.json()
+// }).then((data)=>{
+//     console.log("first ApiAddress:", data);
+// })
+
+
+// let firstApiAddress = "https://jsonplaceholder.typicode.com/todos/1";  //fake api's
+
+// let secondApiAddress = "https://jsonplaceholder.typicode.com/todos/2";
+
+// let ALLApiAddress = "https://jsonplaceholder.typicode.com/todos/";
+
+
+// function FirstApiPromise(){
+//     return fetch(firstApiAddress);      //promise
+
+// }
+
+// async function getFirstApiResult(){
+//     await fetch(firstApiAddress).then((result)=>{
+//         return result.json();
+//     }).then((actualData)=>{
+//         console.log(actualData);
+//     });
+// }
+
+
+// function SecondApiPromise(){
+//     return fetch(firstApiAddress);      //promise
+
+// }
+
+// async function getsecondApiResult(){
+//     await fetch(secondApiAddress).then((result)=>{
+//         return result.json();
+//     }).then((actualData)=>{
+//         console.log(actualData);
+//     });
+// }
+
+
+// function ThirdApiPromise(){
+//     return fetch(firstApiAddress);      //promise
+
+// }
+
+// async function getThirdApiResult(){
+//     await fetch(ALLApiAddress).then((result)=>{
+//         return result.json();
+//     }).then((actualData)=>{
+//         console.log(actualData);
+//     });
+// }
+// Promise.all([FirstApiPromise(), SecondApiPromise(), ThirdApiPromise()]).then((AllResults)=>{
+//     console.log(AllResults);
+
+// })
+//--------------------------------------------------------------------------------------------------
+let firstApiAddress = "https://jsonplaceholder.typicode.com/todos/1";  //fake api's
+
+let secondApiAddress = "https://jsonplaceholder.typicode.com/todos/2";
+
+let ALLApiAddress = "https://jsonplaceholder.typicode.com/todos/";
+
+
+function FirstApi(){
+    return fetch(firstApiAddress);      //promise
+
+}
+function SecondApi(){
+    return fetch(secondApiAddress);     //promise
+    
+}
+
+function ThirdApi(){
+    return fetch(ALLApiAddress);           //promise
+    
+}
+
+async function allApiResults(){
+
+    try{
+    await FirstApi().then((data)=>{
+        return data.json()    //promise
+
+    }).then((data)=>{
+         console.log(data);
+     });
+ }
+ catch(err){
+     console.log(err);
+ } 
+     await SecondApi().then((data)=>{
+         return data.json()    //promise
+
+   }).then((data)=>{
+         console.log(data);
+    });
+
+
+    
+     await ThirdApi().then((data)=>{
+        return data.json()    //promise
+
+    }).then((data)=>{
+        console.log(data);
+     });
+
+}
+allApiResults();
